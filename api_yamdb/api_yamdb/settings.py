0aa1132ab280116b381d82f9dfd445003fcabbe2
Chinilshik-kalkulatorov <biloladkhamov@yandex.ru>
2022-11-26 15:45:06 +0500

    cat dont ordering

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -126,7 +126,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
-}
+
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
